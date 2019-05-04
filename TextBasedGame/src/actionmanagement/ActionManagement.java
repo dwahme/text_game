6bc4ee3b1c2ed1c9d452d32b9b2e95b8a0535ae8
Dawson Whitehead <dwahme1@gmail.com>
2019-05-03 23:52:38 -0500

    Refactored for a better logical separation of handlers

--- TextBasedGame/src/actionmanagement/ActionManagement.java
+++ TextBasedGame/src/actionmanagement/ActionManagement.java
@@ -2,5 +2,25 @@ package actionmanagement;
 
 public class ActionManagement 
 {
+    public ActionManagement()
+    {
 
+    }
+
+    // High level command processor
+    public void process(String input)
+    {
+        if (input.equalsIgnoreCase("INVENTORY"))
+        {
+            System.out.println("You have nothing in your inventory.\n");
+            return;
+        }
+        else if (input.equalsIgnoreCase("LOOK"))
+        {
+            System.out.println("You see an empty room with no doors.\n");
+            return;
+        }
+
+        System.out.println("ACTION MANAGER ERROR- " + input + " not handled");
+    }
 }
--- TextBasedGame/src/cli/CLI.java
+++ TextBasedGame/src/cli/CLI.java
@@ -1,38 +1,65 @@
 package cli;
 
+import actionmanagement.ActionManagement;
+
 public class CLI 
 {
+    ActionManagement Manager = new ActionManagement();
+    String[] commands = {"INVENTORY", "LOOK"};
+    Boolean exit = false;
+    String prompt = "> ";
+
     public CLI() 
     {
-        
-    }
-    //Prints a message to the console
-    public static void WelcomeMessage()
-    {
-        System.out.println("Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. "
-                + "\nIt’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. "
-                + "\nThe dark side of the Force is a pathway to many abilities some consider to be unnatural. "
-                + "\nHe became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. "
-                + "\nUnfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. "
-                + "\nHe could save others from death, but not himself.");
+
     }
 
-    public String process(String input)
+    // Returns if we need to exit the game
+    public Boolean endGame()
     {
-        if (input.equalsIgnoreCase("INVENTORY"))
+        return exit;
+    }
+
+    // Gets the current prompt
+    public void prompt()
+    {
+        System.out.print(prompt);
+    }
+
+    // Sees if a command is valid
+    private Boolean isValidCommand(String input)
+    {
+        for (String command: this.commands)
         {
-            System.out.println("You have nothing in your inventory.\n");
+            if (input.trim().equalsIgnoreCase(command))
+            {
+                return true;
+            }
         }
-        else if (input.equalsIgnoreCase("LOOK"))
+
+        return false;
+    }
+
+    // Processes a command
+    public void process(String input)
+    {
+        // See if the command is one of the baseline commands
+        if (input.equalsIgnoreCase("EXIT"))
         {
-            System.out.println("You see an empty room with no doors.\n");
+            System.out.println("Goodbye, adventurer.\n");
+            this.exit = true;
+            return;
+        }
+
+        // Otherwise, attempt to process the command
+        if (isValidCommand(input))
+        {
+            Manager.process(input);
         }
         else
         {
             System.out.println("Unrecognized command: " + input + "\n");
         }
-
-        return "> ";
     }
     
 }
