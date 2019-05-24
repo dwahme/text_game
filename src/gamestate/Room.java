259eac71fc643b1252461c9aa6b865bafc9aeea1
Dawson Whitehead <dwahme1@gmail.com>
2019-05-24 15:55:26 -0500

    Added function to check if room exists in direction

--- src/gamestate/Room.java
+++ src/gamestate/Room.java
@@ -5,20 +5,33 @@ import java.util.HashMap;
 public class Room 
 {
     HashMap<String, Room> connections = new HashMap<String, Room>();
-    String tmp = "Found!";
 
     //No args constructor
     public Room() 
     {
     }
 
-    // Adds a room to another room's connections
-    public void connectRoom(Room newRoom, String dir, String otherDir)
+    // Sees if a room exists in a certain direction
+    public Boolean roomExists(String dir)
     {
+        return connections.containsKey(dir);
+    }
+
+    // Connects two rooms (returns false if the new room is invalid)
+    public Boolean connectRoom(Room newRoom, String dir, String otherDir)
+    {
+        // Make sure the room is valid
+        if (newRoom == null)
+        {
+            return false;
+        }
+
         // Add the room to the connections
         this.connections.put(dir, newRoom);
 
         // Add this room to the other room's connections
         newRoom.connections.put(otherDir, this);
+
+        return true;
     }
 }
