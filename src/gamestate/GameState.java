bccf26418aaeb788a8ff127d1fd086b15a08d943
Dawson Whitehead <dwahme1@gmail.com>
2019-05-24 15:55:34 -0500

    Removed testing code

--- src/gamestate/GameState.java
+++ src/gamestate/GameState.java
@@ -7,10 +7,5 @@ public class GameState
     //No args constructor
     public GameState() 
     {
-        System.out.println("built game state");
-
-        Room tmpRoom = new Room();
-        currRoom.connectRoom(tmpRoom, "north", "south");
-        System.out.println(tmpRoom.connections.get("south").tmp);
     }
 }
