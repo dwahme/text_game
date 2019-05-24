f9f42a82ebb6a4b95b2f8a4d342e221bff26cdc1
Dawson Whitehead <dwahme1@gmail.com>
2019-05-24 15:45:42 -0500

    Added connecting a room to another room

--- src/gamestate/GameState.java
+++ src/gamestate/GameState.java
@@ -8,6 +8,9 @@ public class GameState
     public GameState() 
     {
         System.out.println("built game state");
+
+        Room tmpRoom = new Room();
+        currRoom.connectRoom(tmpRoom, "north", "south");
+        System.out.println(tmpRoom.connections.get("south").tmp);
     }
-    
 }
--- src/gamestate/Room.java
+++ src/gamestate/Room.java
@@ -1,10 +1,24 @@
 package gamestate;
 
+import java.util.HashMap;
+
 public class Room 
 {
+    HashMap<String, Room> connections = new HashMap<String, Room>();
+    String tmp = "Found!";
+
     //No args constructor
     public Room() 
     {
-        System.out.println("built room");
+    }
+
+    // Adds a room to another room's connections
+    public void connectRoom(Room newRoom, String dir, String otherDir)
+    {
+        // Add the room to the connections
+        this.connections.put(dir, newRoom);
+
+        // Add this room to the other room's connections
+        newRoom.connections.put(otherDir, this);
     }
 }
