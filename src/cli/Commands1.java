7fe424c4b031401a8a1b624cec96a66e9642cd70
Dawson Whitehead <dwahme1@gmail.com>
2019-06-27 23:37:44 -0500

    Implemented valid parameters and wildcards for commands1 and commands2

--- src/cli/Commands1.java
+++ src/cli/Commands1.java
@@ -5,21 +5,47 @@ import java.util.HashSet;
 public class Commands1 extends Commands0
 {
     HashSet<String> param0 = new HashSet<String>();
+    Boolean param0Wildcard = false;
 
     // Constructor with command as argument
     public Commands1(String oneParamCommand, String[] params) 
     {
         super(oneParamCommand);
 
-        for (String param: params)
+        if (params != null)
         {
-            param0.add(param);
+            for (String param: params)
+            {
+                param0.add(param.toUpperCase());
+            }
+        }
+        else
+        {
+            param0Wildcard = true;
         }
     }
 
-    // Sees if a command is valid for this command
-    public Boolean CommandMatches(String candidateCommand)
+    // Sees if a command matches this command's name
+    public Boolean CommandMatches(String args)
     {
-        return super.CommandMatches(candidateCommand);
+        // Split string into arguments
+        String[] splits = args.trim().split("\\s+");
+
+        return super.CommandMatches(splits[0]);
+    }
+
+    // Sees if the parameters are valid
+    public Boolean ValidParams(String args)
+    {
+        // Split string into arguments
+        String[] splits = args.trim().split("\\s+");
+
+        if (splits.length == 2)
+        {
+            return param0Wildcard ||
+                param0.contains(splits[1].toUpperCase());
+        }
+
+        return false;
     }
 }
